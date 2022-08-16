99ce63ea3e80cc473ca63c5ff978991afd7cb89f
user.jasper <user.dekoningjasper@gmail.com>
2022-08-16 16:12:21 +0200

    sim-

--- experiments/jasper_experiments/cost_experiments/setup-experiments.sh
+++ experiments/jasper_experiments/cost_experiments/setup-experiments.sh
@@ -2,7 +2,7 @@
 #set -e
 #set -x
 echo 'je oma ja'
-runs=20
+runs=10
 num_terminals=6
 start_port=8000
 final_gen=199
@@ -59,7 +59,7 @@ while true
          echo ""
          # echo -d -m -S "${experiment}" -L -Logfile "${experiment}.log" -n19 ./revolve.sh --manager "${managers_path}$(cut -d'_' -f1 <<<"$experiment")${managers_sulfix}.py" --experiment-name "${experiments_path}${experiment}" --evaluation-time 50 --n-cores 4 --port-start $start_port
 
-         screen -d -m -S "${experiment}" -L -Logfile "${experiment}.log" nice -n19 ./revolve.sh --manager "${managers_path}$(cut -d'_' -f1 <<<"$experiment")${managers_sulfix}.py" --experiment-name "${experiments_path}${experiment}" --n-cores 4 --port-start $start_port
+         screen -d -m -S "${experiment}" -L -Logfile "${experiment}.log" nice -n19 ./revolve.sh --manager "${managers_path}$(cut -d'_' -f1 <<<"$experiment")${managers_sulfix}.py" --experiment-name "${experiments_path}${experiment}" --n-cores 4 --port-start $start_port --run-simulation 0
 				 start_port=$((${start_port}+10))
 		done
 
