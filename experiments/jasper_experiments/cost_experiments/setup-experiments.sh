c18cdb52fb61d3e60e5bbbdc940fb4a7a2419838
user.jasper <user.dekoningjasper@gmail.com>
2022-08-17 15:11:46 +0200

    status

--- experiments/jasper_experiments/cost_experiments/setup-experiments.sh
+++ experiments/jasper_experiments/cost_experiments/setup-experiments.sh
@@ -57,10 +57,11 @@ while true
     for experiment in "${to_do[@]}"
     do
          echo ""
-         # echo -d -m -S "${experiment}" -L -Logfile "${experiment}.log" -n19 ./revolve.sh --manager "${managers_path}$(cut -d'_' -f1 <<<"$experiment")${managers_sulfix}.py" --experiment-name "${experiments_path}${experiment}" --evaluation-time 50 --n-cores 4 --port-start $start_port
+         # echo -d -m -S "${experiment}" -L -Logfile "${experiment}.log" nice -n19 ./revolve.sh --manager "${managers_path}$(cut -d'_' -f1 <<<"$experiment")${managers_sulfix}.py" --experiment-name "${experiments_path}${experiment}" --evaluation-time 50 --n-cores 4 --port-start $start_port
 
-         screen -d -m -S "${experiment}" -L -Logfile "${experiment}.log"  -n19 ./revolve.sh --manager "${managers_path}$(cut -d'_' -f1 <<<"$experiment")${managers_sulfix}.py" --experiment-name "${experiments_path}${experiment}" --n-cores 4 --port-start $start_port --run-simulation 0
-				 start_port=$((${start_port}+10))    done
+         screen -d -m -S "${experiment}" -L -Logfile "${experiment}.log" nice -n19 ./revolve.sh --manager "${managers_path}$(cut -d'_' -f1 <<<"$experiment")${managers_sulfix}.py" --experiment-name "${experiments_path}${experiment}" --n-cores 4 --port-start $start_port --run-simulation 0
+         start_port=$((${start_port}+10))
+    done
 
     sleep 1800s;
 
