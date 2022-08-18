07b035c66ede597cabd34bccb33d36adb0e9c360
user.jasper <user.dekoningjasper@gmail.com>
2022-08-18 14:32:19 +0200

    fucking _ instead of -

--- experiments/jasper_experiments/cost_experiments/runrelative-experiments.sh
+++ experiments/jasper_experiments/cost_experiments/runrelative-experiments.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-screen -d -m -S killandrun_loop ./experiments/jasper_experiments/cost_experiments/relative_setup-experiments.sh
+screen -d -m -S killandrun_loop ./experiments/jasper_experiments/cost_experiments/relative_setup_experiments.sh
