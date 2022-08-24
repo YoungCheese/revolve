79a0df7d3775e603abf3002e085361df0a2a5304
user.jasper <user.dekoningjasper@gmail.com>
2022-08-24 10:52:19 +0200

    forreal fix print

--- pyrevolve/evolution/fitness.py
+++ pyrevolve/evolution/fitness.py
@@ -144,7 +144,7 @@ def displacement_velocity_cost(behavioural_measurements, robot):
     if behavioural_measurements is not None:
         limit = robot.phenotype.building_diff_unweighted
         speed = behavioural_measurements['displacement_velocity_hill']
-        print(fitness, robot.phenotype.building_diff_unweighted)
+        # print(fitness, robot.phenotype.building_diff_unweighted)
 
         if speed == 0 or robot.phenotype._morphological_measurements.measurements_to_dict()['hinge_count'] == 0:
             return -0.1
