dcc447dcf208390503221f486e5d68f59c06db20
user.jasper <user.dekoningjasper@gmail.com>
2022-08-26 13:24:20 +0200

    costfit

--- experiments/jasper_experiments/cost_experiments/costplasticoding.py
+++ experiments/jasper_experiments/cost_experiments/costplasticoding.py
@@ -85,7 +85,7 @@ async def run():
         next_robot_id = 1
 
     def fitness_function_plane(measures, robot):
-        return fitness.displacement_velocity_hill(measures, robot)
+        return fitness.displacement_velocity_cost(measures, robot)
 
     fitness_function = {'plane': fitness_function_plane,
                         'tilted5': fitness_function_plane} # same function
